4b566c1197a3c13acdcf2f8d69bbe51ff813c061
--global <--global>
2022-11-08 13:47:33 +0500

    my first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
