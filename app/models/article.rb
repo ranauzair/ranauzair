267e50e34891535ad71fc56dadd2274cd02697f6
Ihtisham Ul Haq <ihtisham062@gmail.com>
2022-11-08 14:06:17 +0500

    add comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 
 class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
+    #add comment for check
   validates :title, presence: true,
                     length: { minimum: 5 }
  end
